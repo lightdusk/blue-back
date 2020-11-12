7096dac2a80b7045518e844727766fbc91cac2e6
Unknown <rutgerhol@hotmail.com>
2020-11-12 17:25:11 +0100

    Removed test code

--- routes/api.php
+++ routes/api.php
@@ -21,26 +21,3 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 Route::get("products/{page}/{limit}", [\App\Http\Controllers\ProductController::class,'getProductsPaginated']);
 Route::get("products/count", fn() => json_encode(['count' => DB::table('products')->count()]));
-
-Route::get('test', function() {
-	$client = new \GuzzleHttp\Client();
-	$response = $client->request("GET",
-		"api.tradedoubler.com/1.0/products.json?token=" . env("API_TOKEN"));
-	
-	$data = json_decode($response->getBody(),true);
-	foreach($data['products'] as $entry)
-	{
-		//die( json_encode($entry));
-		$product = new Product();
-		$product->name = $entry['name'];
-		$product->description = $entry['description'];
-		$product->imageURL = $entry['productImage']['url'];
-		$product->shortDescription = $entry['shortDescription'] ?? null;
-		$product->model = $entry['model'] ?? null;
-		$product->brand = $entry['brand'] ?? null;
-		$product->ean = $entry['identifiers']['ean'] ?? null;
-		$product->sku = $entry['identifiers']['sku'] ?? null;
-		$product->save();
-	}
-	return 69;
-});
